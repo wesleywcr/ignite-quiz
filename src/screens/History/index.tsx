4d3ba2390f27a767c0fdab7cd69fa1864c0b6b0b
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2024-03-01 23:01:48 -0300

    refactor: new styles in screen history

--- src/screens/History/index.tsx
+++ src/screens/History/index.tsx
@@ -84,15 +84,15 @@ export function History() {
               }
             }}
               overshootLeft={false} 
-              containerStyle={
-                styles.swipeableContainer
-              }
+              containerStyle={styles.swipeableContainer}
+              leftThreshold={10}
+              renderRightActions={()=>null}
+              onSwipeableOpen={()=>handleRemove(item.id,index)}
               renderLeftActions={()=>(
-              <Pressable 
-              onPress={()=>handleRemove(item.id,index)}
+              <View 
                 style={styles.swipeableRemove}>
                 <Trash size={32} color={THEME.COLORS.GREY_100}/>
-              </Pressable>
+              </View>
           )}
               >
 
