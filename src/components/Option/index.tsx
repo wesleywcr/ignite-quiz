1825cea5a2889a50341546aaf3bcd99c60ad009b
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2024-03-05 22:04:31 -0300

    feat: add animation in component option

--- src/components/Option/index.tsx
+++ src/components/Option/index.tsx
@@ -1,13 +1,36 @@
 import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
 
 import { styles } from './styles';
+import { BlurMask, Canvas, Circle, Easing, Path, Skia, runTiming, useValue } from '@shopify/react-native-skia';
+import { THEME } from '../../styles/theme';
+import { useEffect } from 'react';
 
 type Props = TouchableOpacityProps & {
   checked: boolean;
   title: string;
 }
 
+const CHECK_SIZE = 28
+const CHECK_STROKE = 2
 export function Option({ checked, title, ...rest }: Props) {
+
+  const percentage = useValue(0)
+  const circle = useValue(0)
+
+  const RADIUS =( CHECK_SIZE- CHECK_STROKE)/2
+  const CENTER_CIRCLE = RADIUS/2
+  const path = Skia.Path.Make();
+  path.addCircle(CHECK_SIZE, CHECK_SIZE,RADIUS)
+
+  useEffect(()=>{
+    if(checked){
+      runTiming(percentage,1, {duration:700})
+      runTiming(circle, CENTER_CIRCLE, {easing:Easing.bounce})
+    }else{
+      runTiming(percentage,0, {duration:700})
+      runTiming(circle, 0, {duration:300})
+    }
+  },[checked])
   return (
     <TouchableOpacity
       style={
@@ -21,6 +44,31 @@ export function Option({ checked, title, ...rest }: Props) {
       <Text style={styles.title}>
         {title}
       </Text>
+      <Canvas style={{height:CHECK_SIZE*2, width:CHECK_SIZE*2}}>
+      <Path
+        path={path}
+        color={THEME.COLORS.GREY_600}
+        strokeWidth={CHECK_STROKE}
+   
+      />
+       <Path
+        path={path}
+        color={THEME.COLORS.BRAND_LIGHT}
+        strokeWidth={CHECK_STROKE}
+        start={0}
+        end={percentage}
+      >
+        <BlurMask  blur={1} style={'solid'}/>
+      </Path>
+      <Circle
+      cx={CHECK_SIZE}
+      cy={CHECK_SIZE}
+      r={circle}
+      color={THEME.COLORS.BRAND_LIGHT}
+      >
+      <BlurMask  blur={4} style={'solid'}/>
+      </Circle>
+      </Canvas>
     </TouchableOpacity>
   );
 }
